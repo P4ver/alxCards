80de15d7e8ce0fbf541c32a92bdaa4d9167e8886
P4ver <s.pales20@gmail.com>
2024-08-18 17:28:25 +0100

    adding addCards with user_Login automaticly

--- server/controller/cardController.js
+++ server/controller/cardController.js
@@ -1,4 +1,6 @@
 const pool = require("../db");
+const jwt = require('jsonwebtoken');
+const verifyToken = require("../middleware/verifyToken")
 
 const getCards = (req, res)=>{
     pool.getConnection((err, connection)=>{
@@ -11,4 +13,45 @@ const getCards = (req, res)=>{
     })
 }
 
-module.exports = getCards;
+const addCards = (req, res)=>{
+
+    const {text1, text2, description} = req.body
+    const token = req.cookies.jwt;
+    
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided, authorization denied' });
+    }
+
+    jwt.verify(token, 'my_secret', (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Invalid token cant add card' });
+        }
+        console.log("decoded login_User:",decoded.login_User)
+        const userLogin = decoded.login_User;
+        pool.getConnection((err, connection)=>{
+            if(err) throw err;
+            // connection.query('INSERT INTO flashcards SET?', [req.body],(err, rows)=>{
+            connection.query('INSERT INTO flashcards (text1, text2, description, login_User) VALUES (?, ?, ?, ?)', [text1, text2, description, userLogin],(err, rows)=>{
+                connection.release();
+    
+                if(err) throw err;
+                res.send(rows);
+            })
+        })
+    
+    })
+
+}
+// const addCards = (req, res)=>{
+//     pool.getConnection((err, connection)=>{
+//         if(err) throw err;
+//         connection.query('INSERT INTO flashcards SET?', [req.body],(err, rows)=>{
+//             connection.release();
+
+//             if(err) throw err;
+//             res.send(rows);
+//         })
+//     })
+// }
+
+module.exports = {getCards, addCards};
--- server/routes/cardRoutes.js
+++ server/routes/cardRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const getCards = require('../controller/cardController');
+const {getCards, addCards} = require('../controller/cardController');
 const router = express.Router();
 
 router.get('/cards', getCards)
+router.post('/cards', addCards)
 
 module.exports = router
