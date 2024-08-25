5c1ccb7b950afda081400ccb30b4dad7d9e83e17
P4ver <s.pales20@gmail.com>
2024-08-25 15:40:15 +0100

    add delete route

--- server/routes/cardRoutes.js
+++ server/routes/cardRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {getCards, addCards, getCardsById, updateCards} = require('../controller/cardController');
+const {getCards, addCards, getCardsById, updateCards, deleteCards} = require('../controller/cardController');
 const router = express.Router();
 const verifyToken = require("../middleware/verifyToken")
 
 router.get('/cards', verifyToken, getCards)
-// router.get('/cards', getCards)
 router.get('/cards/:id', verifyToken, getCardsById)
 router.put('/cards/:id', verifyToken, updateCards)
 router.post('/cards', verifyToken, addCards)
+router.delete('/cards/:id', verifyToken, deleteCards);
 
 module.exports = router
